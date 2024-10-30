8705001fa4dc59cefb9ba6ad261792359ee1db5b
JerryLong <longjuncheng@moqipobing.com>
2024-10-30 10:41:06 +0800

    fix: 增加使用demo

--- src/test/java/com/wb/jrl/mock/MockTest.java
+++ src/test/java/com/wb/jrl/mock/MockTest.java
@@ -72,7 +72,7 @@ class MockTest {
     }
 
 
-    private static class MockTestModel {
+    private static class MockTestModel implements Serializable {
         private final String name;
         private final int age;
 
@@ -87,6 +87,13 @@ class MockTest {
 
         public int getAge() {
             return JrlMockUtil.mock(MockTestModel.class, "getAge", () -> age);
+        }
+
+        void run(Runnable runnable) {
+            final JrlMockResponse<Object> run = JrlMockUtil.mock(MockTestModel.class, "run");
+            if (!run.isMock()) {
+                runnable.run();
+            }
         }
     }
 }
